27ba5c25efd2dc14bce2344f6b43b0b7d1f7c48a
dia_maisun <dia_maisun@jcom.home.ne.jp>
2019-09-25 02:23:53 +0900

    .

--- src/New.cpp
+++ src/New.cpp
@@ -5,7 +5,6 @@ using namespace KanameShiki;
 
 
 
-#if NEW//[
 void* operator new(std::size_t size)										{ return Alloc(size); }
 void* operator new[](std::size_t size)										{ return Alloc(size); }
 void operator delete(void* p) noexcept										{ Free(p); }
@@ -15,4 +14,3 @@ void* operator new(std::size_t size, const std::nothrow_t& nt) noexcept		{ retur
 void* operator new[](std::size_t size, const std::nothrow_t& nt) noexcept	{ return Alloc(size); }
 void operator delete(void* p, const std::nothrow_t& nt) noexcept			{ Free(p); }
 void operator delete[](void* p, const std::nothrow_t& nt) noexcept			{ Free(p); }
-#endif//]
