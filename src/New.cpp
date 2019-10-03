39b730774e9363c59ced2dff2e6d76081dd78df2
dia_maisun <dia_maisun@jcom.home.ne.jp>
2019-10-03 21:08:38 +0900

    .

--- src/New.cpp
+++ src/New.cpp
@@ -5,12 +5,12 @@ using namespace KanameShiki;
 
 
 
-void* operator new(std::size_t size)										{ Auto p = Alloc(size); if (!p){ throw std::bad_alloc(); } return p; }
-void* operator new[](std::size_t size)										{ Auto p = Alloc(size); if (!p){ throw std::bad_alloc(); } return p; }
+void* operator new(std::size_t size)										{ Auto p = LocalCntxPtr()->Alloc(size); if (!p){ throw std::bad_alloc(); } return p; }
+void* operator new[](std::size_t size)										{ Auto p = LocalCntxPtr()->Alloc(size); if (!p){ throw std::bad_alloc(); } return p; }
 void operator delete(void* p) noexcept										{ Free(p); }
 void operator delete[](void* p) noexcept									{ Free(p); }
 
-void* operator new(std::size_t size, const std::nothrow_t& nt) noexcept		{ return Alloc(size); }
-void* operator new[](std::size_t size, const std::nothrow_t& nt) noexcept	{ return Alloc(size); }
+void* operator new(std::size_t size, const std::nothrow_t& nt) noexcept		{ return LocalCntxPtr()->Alloc(size); }
+void* operator new[](std::size_t size, const std::nothrow_t& nt) noexcept	{ return LocalCntxPtr()->Alloc(size); }
 void operator delete(void* p, const std::nothrow_t& nt) noexcept			{ Free(p); }
 void operator delete[](void* p, const std::nothrow_t& nt) noexcept			{ Free(p); }
